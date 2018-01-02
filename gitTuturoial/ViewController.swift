562522bd182281e9e26fc2e22f5313fd0e508f4e
宋楠 <fossilsn@fossil.local>
2018-01-02 16:24:12 +0800

    init

---
+++ gitTuturoial/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  gitTuturoial
+//
+//  Created by 宋楠 on 2018/1/2.
+//  Copyright © 2018年 宋楠. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        print ("hello world")
+        super.viewDidLoad()
+
+        
+    }
+
+
+
+
+}
+
